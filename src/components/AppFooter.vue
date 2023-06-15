93244a597f4a79908abc429bd9e7697280e849b6
EmanuelaPau <uapame@gmail.com>
2023-06-15 16:53:46 +0200

    Add footer informations

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,9 +1,48 @@
 <template>
-    <div>
-        <h1>
-            Footer
-        </h1>
-    </div>
+    <footer>
+        <div class="footer-nav">
+
+            <div class="nav-menu">
+                <div class="nav-column">
+                    <div class="nav-col-ul-container">
+                        <h1></h1>
+                        <ul>
+                            <li></li>
+                        </ul>
+                    </div>
+
+                    <div class="nav-col-ul-container">
+                        <h1></h1>
+                        <ul>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="nav-img">
+                <img src="" alt="">
+            </div>
+
+        </div>
+
+        <div class="footer-cta container">
+            <button>Sign-up-now!</button>
+
+            <nav>
+                <a href="">
+                    follow us
+                </a>
+
+                <img src="../assets/footer-facebook.png" alt="#">
+                <img src="../assets/footer-facebook.png" alt="#">
+                <img src="../assets/footer-facebook.png" alt="#">
+                <img src="../assets/footer-facebook.png" alt="#">
+                <img src="../assets/footer-facebook.png" alt="#">
+
+            </nav>
+
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -23,7 +62,41 @@ export default {
 }		
 </script>
 
-<style scoped lang=“scss”>
-	/* @use './styles/general/partials/_variables.scss';
-	@use './styles/general/partials/_mixins.scss'; */
-</style> 
+<style scoped lang="scss">
+@import '../styles/partials/_variables.scss';
+@import '../styles/partials/_mixins.scss';
+
+footer {
+    background-color: $secondaryColor;
+    height: 600px;
+
+    .footer-nav {
+        width: 100%;
+        height: 400px;
+        background-image: url('../assets/footer-bg.jpg');
+        background-repeat: none;
+        background-size: cover;
+    }
+
+    .footer-cta {
+        @include flex(row, space-between, center);
+        padding: 1.3rem;
+
+        nav {
+            @include flex(row, space-between, center);
+
+            a {
+                color: $primaryColor;
+                text-transform: uppercase;
+                margin-right: 12px;
+            }
+
+            img {
+                margin: 7px;
+                width: 30px;
+            }
+        }
+    }
+}
+</style>
+

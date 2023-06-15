8253691a55e81615ffdfec01ac35db1de7ac9b7b
EmanuelaPau <uapame@gmail.com>
2023-06-15 15:45:09 +0200

    Add google font

--- src/App.vue
+++ src/App.vue
@@ -26,5 +26,8 @@ export default {
 
 <style lang=“scss”>
   @import './styles/general.scss';
+
+  /* Google font  */
+  @import url('https://fonts.googleapis.com/css2?family=PT+Sans+Caption:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 </style>
 

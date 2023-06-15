6311e3282d8c2e12077eed8fc81b4bcf8bebeafc
EmanuelaPau <uapame@gmail.com>
2023-06-15 17:36:58 +0200

    add footer contents

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,209 +2,209 @@
     <footer>
         <div class="footer-nav">
 
-            <div class="nav-menu container">
+            <div class="container">
+                <div class="nav-menu">
 
-                <div class="nav-column">
-                    <div class="nav-col-ul-container">
-                        <div class="ul-list-container">
-                            <h3>Dc comics</h3>
-                            <ul>
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
+                    <div class="nav-column">
+                        <div class="nav-col-ul-container">
+                            <div class="ul-list-container">
+                                <h3>Dc comics</h3>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
 
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
 
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
 
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
 
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
 
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
 
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-                            </ul>
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="ul-list-container">
+                                <h3>Shop</h3>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <div class="ul-list-container">
-                            <h3>Shop</h3>
-                            <ul>
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
 
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
+
+                    </div>
+
+                    <div class="nav-column">
+                        <div class="nav-col-ul-container">
+                            <div class="ul-list-container">
+                                <h3>Dc</h3>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="nav-col-ul-container">
+                            <h1></h1>
+                            <ul>
+                                <li></li>
                             </ul>
                         </div>
                     </div>
 
+                    <div class="nav-column">
+                        <div class="nav-col-ul-container">
+                            <div class="ul-list-container">
+                                <h3>Sites</h3>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="">
+                                            Characters
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="nav-col-ul-container">
+                            <h1></h1>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </div>
 
                 </div>
 
-                <div class="nav-column">
-                    <div class="nav-col-ul-container">
-                        <div class="ul-list-container">
-                            <h3>Dc</h3>
-                            <ul>
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="nav-col-ul-container">
-                        <h1></h1>
-                        <ul>
-                            <li></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="nav-column">
-                    <div class="nav-col-ul-container">
-                        <div class="ul-list-container">
-                            <h3>Sites</h3>
-                            <ul>
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="">
-                                        Characters
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="nav-col-ul-container">
-                        <h1></h1>
-                        <ul>
-                            <li></li>
-                        </ul>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="nav-img">
-                <img src="" alt="">
+                <img src="../assets/dc-logo-bg.png" alt="">
             </div>
 
         </div>
@@ -268,42 +268,55 @@ footer {
 
     .footer-nav {
         color: $colorLight;
-        width: 100%;
-        height: 400px;
         background-image: url('../assets/footer-bg.jpg');
         background-repeat: none;
         background-size: cover;
-        padding: 2rem;
+        overflow: hidden;
 
-        .nav-menu {
-            @include flex(row, left, start);
+        div.container {
+            @include flex(row, space-between, center);
+            height: 400px;
 
-            .nav-column {
+            .nav-menu {
+                @include flex(row, left, start);
+                width: 50%;
+                padding: 2rem;
 
-                .nav-col-ul-container {
-                    .ul-list-container {
-                        margin-bottom: 1.3rem;
-                        margin-right: 2.5rem;
+                .nav-column {
 
-                        h3 {
-                            margin-bottom: 1rem;
-                            text-transform: uppercase;
-                        }
+                    .nav-col-ul-container {
+                        .ul-list-container {
+                            margin-bottom: 1.3rem;
+                            margin-right: 2.5rem;
 
-                        ul {
-                            li {
-                                margin-bottom: .5rem;
+                            h3 {
+                                margin-bottom: 1rem;
+                                text-transform: uppercase;
+                            }
 
-                                a {
-                                    color: $tertiaryColor;
+                            ul {
+                                li {
+                                    margin-bottom: .5rem;
+
+                                    a {
+                                        color: $tertiaryColor;
+                                    }
                                 }
                             }
                         }
                     }
                 }
+
+            }
+
+            img {
+                // height: 100%;
+                width: 40%;
+
             }
 
         }
+
 
     }
 
